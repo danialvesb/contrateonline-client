91975492f632e4e289ce8a8b0e26dee2f80a0083
danialvesb <dan78615>
2020-07-13 22:07:03 -0300

    Ajustes no menu

--- src/views/workCalls/workCalls.vue
+++ src/views/workCalls/workCalls.vue
@@ -3,6 +3,11 @@
     <template v-slot:header>
       <ui-header v-on:checkboxToggleHeader="toogleSideBar($event)"/>
     </template>
+    <template v-slot:breadcrumbs>
+      <ui-breadcrumbs>
+        <li>Chamados</li>
+      </ui-breadcrumbs>
+    </template>
     <template v-slot:sidebar>
       <ui-menu-side-bar :sidebar="sidebar"/>
     </template>
@@ -15,6 +20,7 @@
 import dashboardTemplate from '@/templates/page-dashboard/index.vue';
 import { uiMenuSideBar } from '@/components/menu/index';
 import { uiHeader } from '@/components/header/index';
+import { uiBreadcrumbs } from '@components/breadcrumbs/index';
 
 export default {
   name: 'workCalls',
@@ -22,6 +28,7 @@ export default {
     dashboardTemplate,
     uiMenuSideBar,
     uiHeader,
+    uiBreadcrumbs,
   },
   data() {
     return {
